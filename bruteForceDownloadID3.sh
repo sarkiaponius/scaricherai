f9a5e3282ed134b3279fec12596eeade055cc819
sarkiaponius <sarkiaponius@alice.it>
2016-03-18 20:40:46 +0100

    - Dimezzato il chunk scaricato per l'estrazione dei tag (da 2048 a 1024) - Introdotto uno sleep 1 per non affaticare il server - I tag estratti con mid3v2 sono filtrati con insertDB.sh, che produce una riga di insert per il db

--- bruteForceDownloadID3.sh
+++ bruteForceDownloadID3.sh
@@ -23,13 +23,22 @@ START=$1
 STOP=$2
 
 for((i=$START;i<=$STOP;i++)); do 
-	curl -s -r0-2048 "http://www.radio.rai.it/podcast/A$i.mp3" > temp.mp3
-	echo -n "A$i.mp3: "
-	if $(mid3v2 temp.mp3 | grep -q TIT2) ;  then 
-		echo "$(mid3v2 temp.mp3 | grep TIT2 | cut -d= -f2)..."
-		mid3v2 temp.mp3 > A$i.tag
+	MP3="A$i.mp3"
+	ID3="A$i.tag"
+	if [ -e "$ID3" ] ; then
+		echo "$MP3: già scaricato" >&2
 	else
-		echo " scartato"
+		curl -s -r0-1024 "http://www.radio.rai.it/podcast/$MP3" > temp.mp3
+		sleep 1
+		echo -n "$MP3: " >&2
+		if $(mid3v2 temp.mp3 | grep -q TIT2) ;  then 
+			echo "$(mid3v2 temp.mp3 | grep TIT2 | cut -d= -f2)..." >&2
+			mid3v2 temp.mp3 > $ID3
+			./insertDB.sh $ID3
+			rm -f $ID3
+		else
+			echo " scartato" >&2
+		fi
 	fi
 done
 rm -f temp.mp3
